9c6e35bdea3af0e1ec6eb3d619bbf8df057b2f9c
Lucas <lu.orellana@duocuc.cl>
2023-10-27 23:48:08 -0300

    main

--- esc_project/main/static/js/index.js
+++ esc_project/main/static/js/index.js
@@ -1,27 +1,4 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-      delay: 3000, // Intervalo de 2 segundos entre diapositivas
-    },
-    slidesPerView: 2, // Muestra 4 diapositivas a la vez
-    spaceBetween: 30,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      
+console.log("El archivo JavaScript se ha cargado correctamente.");
 
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+
+
