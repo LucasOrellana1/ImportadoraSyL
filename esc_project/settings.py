1c0f7aed9a0f119e2420ecbb394f6e3073d50ef6
Lucas <lu.orellana@duocuc.cl>
2024-01-06 13:19:19 -0300

    vEntorno

--- esc_project/settings.py
+++ esc_project/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get("DEBUG")
-
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['arriendoesc.onrender.com', 'localhost', '127.0.0.1']
